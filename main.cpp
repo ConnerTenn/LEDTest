a2eec6c4b28ed256d321f6b0298e7c157ce1bfd3
ConnerTenn <connertenn@gmail.com>
2019-04-06 20:08:34 -0400

    Working program

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 	RGB colour = {0,0,0};
 	int len = 1;
 	int pin = 18;
-	for (int i = 0; i < argc; i++)
+	for (int i = 1; i < argc; i++)
 	{
 		std::string arg(argv[i]);
 		if (arg=="-l" && i+1<argc)
@@ -20,12 +20,13 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-			u32 col = atoi(argv[i]);
+			u32 col = (int)strtol(argv[i], 0, 0);;
 			colour.R = (col>>8*2)&0xFF;
 			colour.G = (col>>8*1)&0xFF;
 			colour.B = (col>>8*0)&0xFF;
 		}
 	}
+	std::cout << std::hex << (int)colour.R << "," << (int)colour.G << "," << (int)colour.B << std::dec << " [" << len << "] on pin " << pin << "\n";
 	LEDController leds;
 	leds.Init(len, pin);
 	leds.DrawColour(colour);
